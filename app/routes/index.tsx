85bcfa708064fd6d5eb5113adf76b9d916c1bd93
Mariel Pacada <mariel.pickle@gmail.com>
2023-03-21 14:56:25 -0700

    resolved logout functionality

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -24,16 +24,20 @@ export default function Index() {
 
       <div className="landing-text">find your love at first note</div>
 
-      <div className="logout-text full-width-div my-col center-align">
+      <div className="full-width-div my-col center-align">
         <PillButton
           colorClass="green-button"
           text={user ? "login as " + name : "login to spotify"}
           route="/auth/spotify"
         />
+
         {user && (
-          <div className="full-width-div">
-            <p className="inline-child">not you?</p>
-            <Form className="inline-child" action="/logout" method="post">
+          <div className="logout-text">
+            <p>
+              not you? clear your browser's cookies for spotify.com then click
+              logout to clear session.
+            </p>
+            <Form action="/logout" method="post">
               <button className="logout-button">logout</button>
             </Form>
           </div>
