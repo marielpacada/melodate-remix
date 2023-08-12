aa7d467f45e1ebd673f5b85f585900836853d7a0
Mariel Pacada <mariel.pickle@gmail.com>
2023-08-12 15:10:27 -0700

    refactored pill button component

--- app/shared/components/PillButton.tsx
+++ app/shared/components/PillButton.tsx
@@ -1,21 +1,41 @@
 import { Form } from "@remix-run/react";
 
-type PillButtonProps = {
+interface CommonProps {
   colorClass: string;
   text: string;
+}
+
+interface FormSubmitProps extends CommonProps {
+  isSubmit: true;
+  inputName: string;
+  inputValue: any;
+}
+
+interface FormActionProps extends CommonProps {
+  isSubmit: false;
   route: string;
-};
+}
+
+type PillButtonProps = FormSubmitProps | FormActionProps;
 
 export default function PillButton(props: PillButtonProps) {
   return (
     <Form
+      method="post"
+      action={props.isSubmit ? undefined : props.route}
       className={"pill-button-form my-row center-align "
         .concat(props.colorClass)
         .trim()}
-      action={props.route}
-      method="post"
     >
-      <button className="pill-button">{props.text}</button>
+      {props.isSubmit && (
+        <input type="hidden" name={props.inputName} value={props.inputValue} />
+      )}
+      <button
+        className="pill-button"
+        type={props.isSubmit ? "submit" : undefined}
+      >
+        {props.text}
+      </button>
     </Form>
   );
 }
