92a1334eede9f3b48f5792184983792386eb98e7
Mariel Pacada <mariel.pickle@gmail.com>
2023-08-15 13:46:06 -0700

    completed basic match route ui

--- app/routes/match/track.tsx
+++ app/routes/match/track.tsx
@@ -1,10 +1,17 @@
 import type { ActionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getFavoredTracks } from "~/services/match.server";
 import MatchTable from "~/shared/components/MatchTable";
 
 export async function action({ request }: ActionArgs) {
   return null;
 }
 
+export async function loader() {
+  return getFavoredTracks();
+}
+
 export default function Track() {
-  return <MatchTable buttonText="create playlist" />;
+  const data = useLoaderData<typeof loader>();
+  return <MatchTable buttonText="create playlist" records={data} />;
 }
--- app/services/match.server.ts
+++ app/services/match.server.ts
@@ -1,5 +1,8 @@
 import { db } from "./db.server";
 
+/**
+ * Returns Artists that have been swiped right
+ */
 export const getFavoredArtists = async () => {
   const favoredArtists = await db.artist.findMany({
     where: { swiped: true },
@@ -8,4 +11,19 @@ export const getFavoredArtists = async () => {
   return favoredArtists;
 };
 
-// write function for getting tracks
+/**
+ * Return Tracks of the artists that have been swiped right
+ */
+export const getFavoredTracks = async () => {
+  const favoredTracks = [];
+  const trackObjects = await db.artist.findMany({
+    where: { swiped: true },
+    select: { track: true },
+  });
+
+  for (const object of trackObjects) {
+    favoredTracks.push(object["track"]);
+  }
+
+  return favoredTracks;
+};
